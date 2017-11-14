d98de0762dc00e8ecce77ad16ed6596a044a3255
jorgemm7 <jorgemm777@gmail.com>
2017-11-14 17:16:36 +0100

    prueba

--- gitjorge/HelloWorld/src/hello/HellowWorld.java
+++ gitjorge/HelloWorld/src/hello/HellowWorld.java
@@ -4,7 +4,7 @@ public class HellowWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("HeladsadsdasWorld");
     
     }
 }
